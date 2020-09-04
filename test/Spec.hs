b33c2ec9b317b62f3ccad330abc9ccfc0ed1b893
David Marczinus <marczinus.david@gmail.com>
2020-09-04 16:43:31 +0200

    add quickcheck test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,8 @@
 import           Control.Exception (evaluate)
 import           Lib (greet)
 import           Test.Hspec
+import           Test.Hspec.Core.QuickCheck (modifyMaxSuccess)
+import           Test.QuickCheck (property)
 
 main :: IO ()
 main = hspec
@@ -10,3 +12,9 @@ main = hspec
         it "returns hello!"
           $ do
             greet `shouldBe` "hello!"
+    describe "read"
+      $ do
+        modifyMaxSuccess (const 1000)
+          $ it "is inverse to show"
+          $ property
+          $ \x -> (read . show) x == (x :: Int)
